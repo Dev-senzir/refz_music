3a61baea9392e8185964a872c22bf70b748deb3e
SOURCE ELESYED <139072849+githupsource@users.noreply.github.com>
2023-07-29 17:42:41 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/6e4359dc87a11152d5951.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/6e4359dc87a11152d5951.jpg")
+
+SESSION = getenv("SESSION", None)
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/rev_fxx")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/def_Zoka")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
+
+
+FAILED = "https://graph.org/file/6e4359dc87a11152d5951.jpg"
